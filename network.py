02dc3b8816f6773a2e0b77c6c009eefa4e4a4de4
Suske <suske@s19.be>
2024-04-14 01:09:44 +0200

    better client disconnect management

--- network.py
+++ network.py
@@ -22,26 +22,26 @@ class ClientManager():
         # Register the new client socket for monitoring read events
         self.selector.register(client_socket, selectors.EVENT_READ, data=client_address)
 
+    def _close_wrapper(self, client_socket):
+        self.selector.unregister(client_socket)
+        client_socket.close()
+    
     def _handle_client(self, client_socket, message):
         try:
             client_socket.sendall(message)
         except Exception as e:
-            print('error occurred:', e)
-            # Unregister and close the connection on error
-            self.selector.unregister(client_socket)
-            client_socket.close()
+            print('error occurred:', e) 
     
     def send(self, msg):
-        events = self.selector.select(timeout=0)  # Timeout set to 1 second
-        print(events)
-        
-        for key, mask in events:
-            if key.fileobj == self.server_socket:
-                self._accept_wrapper(key.fileobj)
+        while len((events := self.selector.select(timeout=0))):
+            for selector_key, _ in events:
+                if selector_key.fileobj == self.server_socket:
+                    self._accept_wrapper(selector_key.fileobj)
+                else:
+                    self._close_wrapper(selector_key.fileobj)
 
-        for key, mask in self.selector.get_map().items():
-            if mask.data is not None:  # Check if the key has client data
-                client_socket = mask.fileobj
-                client_address = mask.data
+        for _, selector_key in self.selector.get_map().items():
+            if selector_key.data is not None:  # Check if the key has client data
+                client_socket = selector_key.fileobj
                 self._handle_client(client_socket, bytes(msg, encoding='utf-8'))
 
